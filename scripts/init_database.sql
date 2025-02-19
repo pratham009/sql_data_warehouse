e81089c46ed438e41b291a1ff4e409b704fce4ab
Pratham Nayan Vichare <66873960+pratham009@users.noreply.github.com>
2025-02-19 14:16:53 -0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,26 @@
+-- This script creates a data warehouse database named 'Datawareh' with three schemas: bronze, silver, and gold.
+-- It first checks if the database exists, and if so, drops it before recreating it.
+-- Use this when setting up or resetting the data warehouse.
+
+
+USE master;
+GO 
+
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name='Datawareh')
+BEGIN 
+    ALTER DATABASE Datawareh SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Datawareh;
+END;
+GO 
+
+CREATE DATABASE Datawareh;
+GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
 
